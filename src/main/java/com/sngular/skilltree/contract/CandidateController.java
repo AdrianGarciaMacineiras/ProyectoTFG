0febec52dfed67188718ea2ab2e582d4542bd22f
Adrian <adrian.garciam@udc.es>
2022-12-30 10:19:24 +0100

    CandidateController + ClientController + Typos skill-tree-api.yml

--- src/main/java/com/sngular/skilltree/contract/CandidateController.java
+++ src/main/java/com/sngular/skilltree/contract/CandidateController.java
@@ -1,10 +1,65 @@
 package com.sngular.skilltree.contract;
 
+import com.sngular.skilltree.api.CandidateApi;
+import com.sngular.skilltree.api.model.CandidateDTO;
+import com.sngular.skilltree.api.model.PatchedCandidateDTO;
+import com.sngular.skilltree.application.CandidateService;
+import com.sngular.skilltree.contract.mapper.CandidateMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class CandidateController {
+public class CandidateController implements CandidateApi {
 
+    private final CandidateService candidateService;
+
+    private final CandidateMapper candidateMapper;
+
+    @Override
+    public ResponseEntity<List<CandidateDTO>> getCandidates() {
+        var candidateList = candidateService.getAll();
+        return ResponseEntity.ok(candidateMapper.toCandidatesDTO(candidateList));
+    }
+
+    @Override
+    public ResponseEntity<CandidateDTO> addCandidate(CandidateDTO candidateDTO) {
+        return ResponseEntity.ok(candidateMapper
+                .toCandidateDTO(candidateService
+                        .create(candidateMapper
+                                .toCandidate(candidateDTO))));
+    }
+
+    @Override
+    public ResponseEntity<CandidateDTO> getCandidateByCode(String candidatecode) {
+        return ResponseEntity.ok(candidateMapper
+                .toCandidateDTO(candidateService
+                        .findByCode(candidatecode)));
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCandidate(String candidatecode) {
+        var result = candidateService.deleteBeCode(candidatecode);
+        return ResponseEntity.status(result? HttpStatus.OK: HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @Override
+    public ResponseEntity<CandidateDTO> updateCandidate(String candidatecode, CandidateDTO candidateDTO) {
+        return ResponseEntity.ok(candidateMapper
+                .toCandidateDTO(candidateService
+                        .update(candidatecode, candidateMapper
+                                .toCandidate(candidateDTO))));
+    }
+
+    @Override
+    public ResponseEntity<CandidateDTO> patchCandidate(String candidatecode, PatchedCandidateDTO patchedCandidateDTO) {
+        return ResponseEntity.ok(candidateMapper
+                .toCandidateDTO(candidateService
+                        .patch(candidatecode, candidateMapper
+                                .toCandidate(patchedCandidateDTO))));
+    }
 }
--- src/main/java/com/sngular/skilltree/contract/ClientController.java
+++ src/main/java/com/sngular/skilltree/contract/ClientController.java
@@ -1,15 +1,65 @@
 package com.sngular.skilltree.contract;
 
+import com.sngular.skilltree.api.ClientApi;
+import com.sngular.skilltree.api.model.ClientDTO;
+import com.sngular.skilltree.api.model.PatchedClientDTO;
 import com.sngular.skilltree.application.ClientService;
 import com.sngular.skilltree.contract.mapper.ClientMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class ClientController {
+public class ClientController implements ClientApi {
 
     private final ClientService clientService;
 
     private final ClientMapper clientMapper;
+
+    @Override
+    public ResponseEntity<List<ClientDTO>> getClients() {
+        var clientList = clientService.getAll();
+        return ResponseEntity.ok(clientMapper.toClientsDTO(clientList));
+    }
+
+    @Override
+    public ResponseEntity<ClientDTO> addClient(ClientDTO clientDTO) {
+        return ResponseEntity.ok(clientMapper
+                .toClientDTO(clientService
+                        .create(clientMapper
+                                .toClient(clientDTO))));
+    }
+
+    @Override
+    public ResponseEntity<ClientDTO> getClientByCode(String clientcode) {
+        return ResponseEntity.ok(clientMapper
+                .toClientDTO(clientService
+                        .findByCode(clientcode)));
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteClient(String clientcode) {
+        var result = clientService.deleteBeCode(clientcode);
+        return ResponseEntity.status(result? HttpStatus.OK: HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @Override
+    public ResponseEntity<ClientDTO> updateClient(String clientcode, ClientDTO clientDTO) {
+        return ResponseEntity.ok(clientMapper
+                .toClientDTO(clientService
+                        .update(clientcode, clientMapper
+                                .toClient(clientDTO))));
+    }
+
+    @Override
+    public ResponseEntity<ClientDTO> patchClient(String clientcode, PatchedClientDTO patchedClientDTO) {
+        return ResponseEntity.ok(clientMapper
+                .toClientDTO(clientService
+                        .patch(clientcode, clientMapper
+                                .toClient(patchedClientDTO))));
+    }
 }
