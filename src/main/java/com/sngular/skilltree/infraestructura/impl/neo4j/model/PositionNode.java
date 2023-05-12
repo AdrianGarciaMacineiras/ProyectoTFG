2ef019f5823c64e02a4587cd35c83b50e9261941
Adrian <adrian.garciam@udc.es>
2023-05-12 13:12:12 +0200

    Sorting

--- src/main/java/com/sngular/skilltree/infraestructura/impl/neo4j/model/PositionNode.java
+++ src/main/java/com/sngular/skilltree/infraestructura/impl/neo4j/model/PositionNode.java
@@ -19,11 +19,9 @@ public class PositionNode {
 
     private String name;
 
-    @Relationship(type="FOR_PROJECT", direction = Relationship.Direction.OUTGOING)
-    private ProjectNode project;
+    private boolean deleted;
 
-    @Relationship(type="FOR_CLIENT", direction = Relationship.Direction.OUTGOING)
-    private ClientNode client;
+    private boolean open;
 
     private LocalDate openingDate;
 
@@ -33,13 +31,19 @@ public class PositionNode {
 
     private EnumMode mode;
 
+    private String role;
+
+    @Relationship(type="FOR_PROJECT", direction = Relationship.Direction.OUTGOING)
+    private ProjectNode project;
+
+    @Relationship(type="FOR_CLIENT", direction = Relationship.Direction.OUTGOING)
+    private ClientNode client;
+
     @Relationship(type="IN_THE_OFFICE", direction = Relationship.Direction.OUTGOING)
     private OfficeNode office;
 
     @Relationship(type="MANAGED_BY", direction = Relationship.Direction.OUTGOING)
     private PeopleNode managedBy;
-
-    private String role;
 
     @Relationship(type="NEEDS", direction = Relationship.Direction.OUTGOING)
     private List<PositionSkillsRelationship> skills;
@@ -47,8 +51,5 @@ public class PositionNode {
     @Relationship(type = "CANDIDATE", direction = Relationship.Direction.OUTGOING)
     private List<CandidateRelationship> candidates;
 
-    private boolean deleted;
-
-    private boolean open;
 
 }
