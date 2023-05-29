a27e20dd1a972f123a1e88074e9a66ca23a82e41
Adrian <adrian.garciam@udc.es>
2023-05-29 13:00:35 +0200

    Struggling testAssignCandidate()

--- src/test/java/com/sngular/skilltree/application/CandidateServiceTest.java
+++ src/test/java/com/sngular/skilltree/application/CandidateServiceTest.java
@@ -1,23 +1,26 @@
 package com.sngular.skilltree.application;
 
-import static com.sngular.skilltree.application.CandidateFixtures.CANDIDATE2_BY_CODE;
-import static com.sngular.skilltree.application.CandidateFixtures.CANDIDATE_BY_CODE;
-import static com.sngular.skilltree.application.CandidateFixtures.CANDIDATE_LIST;
+import static com.sngular.skilltree.application.CandidateFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.InstanceOfAssertFactories.list;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.sngular.skilltree.application.implement.CandidateServiceImpl;
 import com.sngular.skilltree.infraestructura.CandidateRepository;
 import com.sngular.skilltree.infraestructura.PositionRepository;
-import com.sngular.skilltree.model.Candidate;
+import com.sngular.skilltree.model.*;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -30,7 +33,11 @@ class CandidateServiceTest {
     @Mock
     private PositionRepository positionRepository;
 
+    @Mock
     private CandidateService candidateService;
+
+    @Captor
+    ArgumentCaptor<List<Candidate>> listArgumentCaptor;
 
     @BeforeEach
     void setUp() {candidateService = new CandidateServiceImpl(candidateRepository, positionRepository);}
@@ -59,4 +66,39 @@ class CandidateServiceTest {
         boolean result = candidateService.deleteByCode("c1120");
         assertThat(result).isTrue();
     }
+
+    @Test
+    @DisplayName("Testing generate candidates")
+    void testGenerateCandidates(){
+        when(candidateRepository.generateCandidates(anyString(), anyList())).thenReturn(CANDIDATE_LIST);
+        List<Candidate> result = candidateService.generateCandidates("itxtl1", List.of(POSITION_SKILL));
+        assertThat(result).containsExactly(CANDIDATE_BY_CODE, CANDIDATE2_BY_CODE);
+    }
+
+
+    @Test
+    @DisplayName("Testing assign candidate")
+    void testAssignCandidate(){
+        doNothing().when(candidateRepository).assignCandidate(anyString(),anyLong(),listArgumentCaptor.capture());
+        candidateService.assignCandidate("itxtl1", 1L);
+        assertThat(listArgumentCaptor.getValue()).containsExactly(CANDIDATE_BY_CODE, CANDIDATE2_BY_CODE);
+    }
+
+    @Test
+    @DisplayName("Testing get candidates using long as parameter")
+    void testGetCandidatesLong(){
+        when(candidateRepository.getCandidates(anyLong())).thenReturn(CANDIDATE_LIST);
+        List<Candidate> result = candidateService.getCandidates(1L);
+        assertThat(result).containsExactly(CANDIDATE_BY_CODE, CANDIDATE2_BY_CODE);
+    }
+
+    @Test
+    @DisplayName("Testing get candidates using string as parameter")
+    void testGetCandidatesString(){
+        when(candidateRepository.getCandidates(anyString())).thenReturn(CANDIDATE_LIST);
+        List<Candidate> result = candidateService.getCandidates("itxtl1");
+        assertThat(result).containsExactly(CANDIDATE_BY_CODE, CANDIDATE2_BY_CODE);
+    }
+
+
 }
