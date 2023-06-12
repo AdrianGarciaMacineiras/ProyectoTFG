96f10270f29b088d7f24c4b245066357a8f22e09
Adrian <adrian.garciam@udc.es>
2023-06-12 11:54:00 +0200

    Service Tests

--- src/main/java/com/sngular/skilltree/infraestructura/impl/neo4j/mapper/TeamNodeMapper.java
+++ src/main/java/com/sngular/skilltree/infraestructura/impl/neo4j/mapper/TeamNodeMapper.java
@@ -6,6 +6,8 @@ import com.sngular.skilltree.common.config.CommonMapperConfiguration;
 import com.sngular.skilltree.infraestructura.impl.neo4j.ResolveServiceTeamNode;
 import com.sngular.skilltree.infraestructura.impl.neo4j.model.MemberRelationship;
 import com.sngular.skilltree.infraestructura.impl.neo4j.model.TeamNode;
+import com.sngular.skilltree.infraestructura.impl.neo4j.projection.MemberRelationshipProjection;
+import com.sngular.skilltree.infraestructura.impl.neo4j.projection.TeamProjection;
 import com.sngular.skilltree.model.Member;
 import com.sngular.skilltree.model.Team;
 import org.mapstruct.Mapper;
@@ -22,5 +24,8 @@ public interface TeamNodeMapper {
 
     List<Team> map(List<TeamNode> all);
 
+    List<Team> mapProjection(List<TeamProjection> all);
+
+    List<Member> mapMemberRelationshipProjection(List<MemberRelationshipProjection> memberRelationshipProjections);
 
 }
