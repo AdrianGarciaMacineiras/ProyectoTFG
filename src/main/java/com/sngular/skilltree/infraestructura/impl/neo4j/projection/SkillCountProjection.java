ca7f55740b7bcaac4e50e413ed86a11451f6613f
Jose Enrique García Maciñeiras <joseenrique.garcia@sngular.com>
2023-08-10 16:57:45 +0200

    Improve Skills

--- src/main/java/com/sngular/skilltree/infraestructura/impl/neo4j/projection/SkillCountProjection.java
+++
@@ -1,7 +0,0 @@
-package com.sngular.skilltree.infraestructura.impl.neo4j.projection;
-
-import lombok.Builder;
-
-@Builder
-public record SkillCountProjection(String parent, String skill, Integer total) {
-}
