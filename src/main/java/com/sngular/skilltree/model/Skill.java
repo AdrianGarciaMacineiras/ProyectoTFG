e12188868b7199ee69971d7bc8fae042f0ed28da
Jose Enrique García Maciñeiras <joseenrique.garcia@sngular.com>
2023-08-16 15:10:15 +0200

    Fix Skill Issue

--- src/main/java/com/sngular/skilltree/model/Skill.java
+++ src/main/java/com/sngular/skilltree/model/Skill.java
@@ -1,14 +1,13 @@
 package com.sngular.skilltree.model;
 
 import lombok.Builder;
-import lombok.Singular;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.text.WordUtils;
+import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Value
 @Slf4j
@@ -19,7 +18,6 @@ public class Skill {
 
     String name;
 
-    @Singular("subSkill")
     Map<String, Skill> subSkillList;
 
     public void addSkill(final String parent, final Skill skill) {
@@ -45,5 +43,35 @@ public class Skill {
 
     public List<Skill> subSkills() {
         return new ArrayList<>(subSkillList.values());
+    }
+
+    public static class SkillBuilder {
+        private final Map<String, Skill> subSkillList = new HashMap<>();
+
+        public SkillBuilder subSkillList(final Map<String, Skill> subSkillList) {
+            if (!CollectionUtils.isEmpty(subSkillList)) {
+                this.subSkillList.putAll(subSkillList);
+            }
+            return this;
+        }
+
+        public SkillBuilder subSkill(final Skill subSkill) {
+            if (!Objects.nonNull(subSkill)) {
+                this.subSkillList.put(subSkill.getCode(), subSkill);
+            }
+            return this;
+        }
+
+        public SkillBuilder subSkill(final String key, final Skill subSkillList) {
+            if (!ObjectUtils.allNotNull(key, subSkillList)) {
+                this.subSkillList.put(key, subSkillList);
+            }
+            return this;
+        }
+
+        public SkillBuilder clearSubSkillList() {
+            this.subSkillList.clear();
+            return this;
+        }
     }
 }
